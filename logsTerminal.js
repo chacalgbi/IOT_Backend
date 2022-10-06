e571c28f636b9405da08c687e05d5e01879e250f
Thomé Lucas de Araújo Oliveira <chacalgbi@hotmail.com>
2022-10-05 21:01:56 -0300

    Add logs de dispositivo OffLine

--- logsTerminal.js
+++ logsTerminal.js
@@ -1,9 +1,10 @@
 const API = require('./utils/API')
 const log = require('./utils/log')
+const timer = require('./utils/dataHora')
 const ChannelModel = require('./models/channels')
 const mqtt = require('mqtt')
-const client = mqtt.connect('wss://narrowbane896:jhUlUA907SSh225C@narrowbane896.cloud.shiftr.io', { clientId: 'Server' })
-
+const client = mqtt.connect('wss://narrowbane896:jhUlUA907SSh225C@narrowbane896.cloud.shiftr.io', { clientId: 'Server_Local' })
+let ArrayAtivo = []
 
 async function channels_list(topic, message) {
     log(`Procura Terminal ${topic}`, 'info')
@@ -39,6 +40,29 @@ async function channels_list(topic, message) {
 
 }
 
+function aviso_placa_offline(topico, msg){
+    //console.log(topico, msg)
+    ArrayAtivo.map((a) => {
+        if (a.topico === topico) {
+
+            if(msg === '1') {
+                clearTimeout(a.func)
+            } else if(msg === '0') {
+                clearTimeout(a.func)
+            }
+
+            // Seta de novo o TimeOut, caso a placa fique Offline, essa função dentro vai executar
+            a.func = setTimeout(() => {
+                const newTopic = topico.replace("ativo", "terminal_OUT")
+                const payload = `\n${timer()}Offline`
+                //console.log(`----------------------------${newTopic} - ${payload}`)
+                client.publish(newTopic, payload)
+            }, 30000)
+
+        }
+    })
+}
+
 function subscrever(terminais) {
     //client.end()
     client.on('connect', function () {
@@ -54,9 +78,17 @@ function subscrever(terminais) {
     })
 
     client.on('message', function (topic, message) {
-        console.log(topic.toString())
-        //console.log(message.toString())
-        channels_list(topic.toString(), message.toString())
+        const topico = topic.toString()
+        const msg = message.toString()
+
+        //console.log(topico)
+        //console.log(msg)
+
+        if(topico.includes("ativo")){
+            aviso_placa_offline(topico, msg)
+        }else if(topico.includes("terminal_view")){
+            channels_list(topico, msg)
+        }
     })
 }
 
@@ -66,9 +98,15 @@ module.exports = async function logsTerminal() {
     await ChannelModel.findAll()
         .then((res) => {
             let terminais = []
+            let ativos = []
             res.map((i) => {
-                if (i.type === 'terminal_view') {
+                if (i.type === 'terminal_view' || i.type === 'ativo') {
                     terminais.push(i.path)
+                }
+
+                //Guarda no array a lista path ativos
+                if(i.type === 'ativo'){
+                    ArrayAtivo.push({ topico: i.path, func: null })
                 }
             })
             subscrever(terminais)
